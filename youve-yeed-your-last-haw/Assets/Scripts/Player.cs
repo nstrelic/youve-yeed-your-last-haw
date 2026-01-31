ef6fd01a548403f92b3d6a6ef4570bd688915f83
nickstrelic <nstrelic@gmail.com>
2026-01-30 21:42:07 -0600

    key codes

---
+++ youve-yeed-your-last-haw/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    int playerNumber = 1;
+
+    KeyCode drawKey = KeyCode.A;
+    KeyCode battleKey = KeyCode.S;
+
+    GameManager gameManager;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        drawKey = gameManager.dynamicPossibleKeyCodes[Random.Range(0, gameManager.dynamicPossibleKeyCodes.Count)];
+        gameManager.dynamicPossibleKeyCodes.Remove(drawKey);
+        battleKey = gameManager.dynamicPossibleKeyCodes[Random.Range(0, gameManager.dynamicPossibleKeyCodes.Count)];
+        gameManager.dynamicPossibleKeyCodes.Remove(battleKey);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
